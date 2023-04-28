ad54b568af8e4ae14fa94a14931dbb853ef121c1
Oyuki-kintaro <lydia_k_u@yahoo.com>
2023-04-28 00:19:41 -0500

    draft logic

---
+++ TicTacToe.py
@@ -0,0 +1,23 @@
+
+
+# print menu
+#   Choose your opponent? Computer(c) or player2(p) or exit(q)?
+
+# recieve input from user
+
+# check string matches options
+
+# deny and reask if not match options
+# else start game
+
+# print game layout and clarify choosing number to place sign
+# and ask player1 to choose
+
+#recieve input, update value with on game board
+
+#check if there are 3 in a row
+# if yes, whichever player wins if no next player turn
+
+#logic for computer if chosen as player
+
+# repeat until winner
